a1e72272c4417fa54ef76a422605d02d0da07438
Nick van Bokhorst <nickvanbokhorst@hotmail.com>
2019-06-13 11:30:35 +0200

    beryllium: Move to AnyKernel3

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -1,9 +1,9 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # begin properties
-properties() {
+properties() { '
 kernel.string=Optimus Drunk Kernel by GtrCraft
 do.devicecheck=1
 do.modules=0
@@ -15,27 +15,30 @@ device.name3=
 device.name4=
 device.name5=
 supported.versions=
-} # end properties
+'; } # end properties
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
+
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. tools/ak3-core.sh;
 
-## AnyKernel permissions
-# set permissions for included ramdisk files
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
+
 
 ## AnyKernel install
 dump_boot;
 
+# init.rc
 backup_file init.rc;
-
 insert_line init.rc "init.optimus.rc" after "import /init.usb.configfs.rc" "import /init.optimus.rc";
 
 # Display if using MIUI or a custom ROM
@@ -47,6 +50,5 @@ else
 fi
 
 write_boot;
-
 ## end install
 
