cc1120f1a015e6a604dee0bd6e2910ba686f672f
GtrCraft <nickvanbokhorst@hotmail.com>
2019-09-30 19:51:51 +0200

    beryllium: AnyKernel3: Make AK3 compatible with SAR
    Change-Id: I17e1d54505976b263a89e861c0e5b551616a94d2

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -19,7 +19,7 @@ supported.versions=
 
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+is_slot_device=auto;
 ramdisk_compression=auto;
 
 
@@ -30,8 +30,7 @@ ramdisk_compression=auto;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
-chown -R root:root $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/*;
 
 
 ## AnyKernel install
@@ -40,9 +39,10 @@ dump_boot;
 # Begin Ramdisk Changes
 . /tmp/anykernel/tools/fstab.sh;
 
-# init.rc
-backup_file init.rc;
-insert_line init.rc "init.optimus.rc" after "import /init.usb.configfs.rc" "import /init.optimus.rc";
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
 
 write_boot;
 ## end install
