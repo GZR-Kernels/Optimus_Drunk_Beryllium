9932e9776b25f5b4d03b705a8c2d7387ae275e83
Pierre2324 <pdbbasketball@gmail.com>
2019-11-03 09:52:12 -0500

    klapse: fix compiling missing endif issue
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
+++ drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
@@ -25,6 +25,7 @@ unsigned short kcal_blue = 256;
 static unsigned short kcal_red = 256;
 static unsigned short kcal_green = 256;
 static unsigned short kcal_blue = 256;
+#endif
 static unsigned short kcal_hue = 0;
 static unsigned short kcal_sat = 255;
 static unsigned short kcal_val = 255;
