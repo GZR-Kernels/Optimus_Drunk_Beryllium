c54cdd20c0070acfa6d9410a37ed7d6bd5a95d1f
GtrCraft <nickvanbokhorst@hotmail.com>
2020-05-18 08:01:49 +0200

    beryllium: Add build script

---
+++ build.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+#set -e
+
+## Copy this script inside the kernel directory
+CLANG_TOOLCHAIN=$KERNELDIR/prebuilts/clang-6443078/bin/clang-11
+KERNEL_TOOLCHAIN=$KERNELDIR/prebuilts/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+ARM32_TOOLCHAIN=$KERNELDIR/prebuilts/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+KERNEL_DEFCONFIG=beryllium_defconfig
+ANYKERNEL3_DIR=$PWD/AnyKernel3/
+FINAL_KERNEL_ZIP=Optimus_Drunk_Beryllium_v10.19.zip
+# Speed up build process
+MAKE="./makeparallel"
+
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+echo "**** Setting Toolchain ****"
+export CROSS_COMPILE=$KERNEL_TOOLCHAIN
+export CROSS_COMPILE_ARM32=$ARM32_TOOLCHAIN
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_COMPILER_STRING="Clang Version 11.0.1"
+
+# Clean build always lol
+echo "**** Cleaning ****"
+mkdir -p out
+make O=out clean
+
+echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+echo -e "$blue***********************************************"
+echo "          BUILDING KERNEL          "
+echo -e "***********************************************$nocol"
+make $KERNEL_DEFCONFIG O=out
+make -j$(nproc --all) CC=$CLANG_TOOLCHAIN CLANG_TRIPLE=aarch64-linux-gnu- O=out
+
+echo "**** Verify Image.gz-dtb ****"
+ls $PWD/out/arch/arm64/boot/Image.gz-dtb
+
+#Anykernel 2 time!!
+echo "**** Verifying AnyKernel3 Directory ****"
+ls $ANYKERNEL3_DIR
+echo "**** Removing leftovers ****"
+rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+
+echo "**** Copying Image.gz-dtb ****"
+cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/
+
+echo "**** Time to zip up! ****"
+cd $ANYKERNEL3_DIR/
+zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
+cp $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP $KERNELDIR/$FINAL_KERNEL_ZIP
+
+echo "**** Done, here is your sha1 ****"
+cd ..
+rm -rf $ANYKERNEL3_DIR/$FINAL_KERNEL_ZIP
+rm -rf $ANYKERNEL3_DIR/Image.gz-dtb
+rm -rf out/
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+sha1sum $KERNELDIR/$FINAL_KERNEL_ZIP
