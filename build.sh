bb5c189c7f1f7ac59e716282b293126616bf7173
GtrCraft <nickvanbokhorst@hotmail.com>
2020-10-02 11:04:57 +0200

    Optimus Drunk Beryllium v10.35

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 ## Copy this script inside the kernel directory
 KERNEL_DEFCONFIG=beryllium_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=Optimus_Drunk_Beryllium_v10.34.zip
+FINAL_KERNEL_ZIP=Optimus_Drunk_Beryllium_v10.35.zip
 export PATH="$KERNELDIR/prebuilts/proton-clang/bin:${PATH}"
 export ARCH=arm64
 export SUBARCH=arm64
