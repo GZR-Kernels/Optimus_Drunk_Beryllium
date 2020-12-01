9e1b3f460bc0092c5d227dd3f84eb156d595acca
GtrCraft <nickvanbokhorst@hotmail.com>
2020-12-01 21:48:57 +0100

    beryllium: Remove cpu input & devfreq boost

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -183,8 +183,6 @@ struct devfreq {
 
 	unsigned long min_freq;
 	unsigned long max_freq;
-	bool is_boost_device;
-	bool max_boost;
 	bool stop_polling;
 
 	/* information for device frequency transition */
@@ -316,9 +314,6 @@ struct devfreq_passive_data {
 };
 #endif
 
-/* Caution: devfreq->lock must be locked before calling update_devfreq */
-extern int update_devfreq(struct devfreq *devfreq);
-
 #else /* !CONFIG_PM_DEVFREQ */
 static inline struct devfreq *devfreq_add_device(struct device *dev,
 					  struct devfreq_dev_profile *profile,
@@ -421,11 +416,6 @@ static inline struct devfreq *devfreq_get_devfreq_by_phandle(struct device *dev,
 }
 
 static inline int devfreq_update_stats(struct devfreq *df)
-{
-	return -EINVAL;
-}
-
-static inline int update_devfreq(struct devfreq *devfreq)
 {
 	return -EINVAL;
 }
