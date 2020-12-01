36786d5d55e1a0245b6c2f690851e9fe84df3252
GtrCraft <nickvanbokhorst@hotmail.com>
2020-12-01 21:13:43 +0100

    beryllium: Remove dynamic stune boost

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -44,16 +44,6 @@ module_param(sched_boost_on_input, uint, 0644);
 
 static bool sched_boost_active;
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static int dynamic_stune_boost;
-module_param(dynamic_stune_boost, uint, 0644);
-static bool stune_boost_active;
-static int boost_slot;
-static unsigned int dynamic_stune_boost_ms = 40;
-module_param(dynamic_stune_boost_ms, uint, 0644);
-static struct delayed_work dynamic_stune_boost_rem;
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
-
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
 #define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
@@ -197,37 +187,16 @@ static void do_input_boost_rem(struct work_struct *work)
 	}
 }
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-static void do_dynamic_stune_boost_rem(struct work_struct *work)
-{
-	/* Reset dynamic stune boost value to the default value */
-	if (stune_boost_active) {
-		reset_stune_boost("top-app", boost_slot);
-		stune_boost_active = false;
-	}
-}
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
-
 static void do_input_boost(struct work_struct *work)
 {
 	unsigned int i, ret;
 	struct cpu_sync *i_sync_info;
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	cancel_delayed_work_sync(&dynamic_stune_boost_rem);
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 	cancel_delayed_work_sync(&input_boost_rem);
 	if (sched_boost_active) {
 		sched_set_boost(0);
 		sched_boost_active = false;
 	}
-
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	if (stune_boost_active) {
-		reset_stune_boost("top-app", boost_slot);
-		stune_boost_active = false;
-	}
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	/* Set the input_boost_min for all CPUs in the system */
 	pr_debug("Setting input boost min for all CPUs\n");
@@ -247,16 +216,6 @@ static void do_input_boost(struct work_struct *work)
 		else
 			sched_boost_active = true;
 	}
-
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	/* Set dynamic stune boost value */
-	ret = do_stune_boost("top-app", dynamic_stune_boost, &boost_slot);
-	if (!ret)
-		stune_boost_active = true;
-
-	queue_delayed_work(cpu_boost_wq, &dynamic_stune_boost_rem,
-					msecs_to_jiffies(dynamic_stune_boost_ms));
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	queue_delayed_work(cpu_boost_wq, &input_boost_rem,
 					msecs_to_jiffies(input_boost_ms));
@@ -313,11 +272,6 @@ err2:
 
 static void cpuboost_input_disconnect(struct input_handle *handle)
 {
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	/* Reset dynamic stune boost value to the default value */
-	reset_stune_boost("top-app", boost_slot);
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
-
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
@@ -368,9 +322,6 @@ static int cpu_boost_init(void)
 
 	INIT_WORK(&input_boost_work, do_input_boost);
 	INIT_DELAYED_WORK(&input_boost_rem, do_input_boost_rem);
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
-	INIT_DELAYED_WORK(&dynamic_stune_boost_rem, do_dynamic_stune_boost_rem);
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	for_each_possible_cpu(cpu) {
 		s = &per_cpu(sync_info, cpu);
